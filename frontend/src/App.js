82548e52b542e6819a6c738e6e892517e1b70578
unknown <sudisimbadav@gmail.com>
2021-03-17 08:27:19 +0200

    Completed the Product Landing Page and Product Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import './App.css'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to SimbaShop</h1>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
