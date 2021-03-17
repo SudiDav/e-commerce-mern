354756dc6cddfe7850883f1900e9bfad95d1edc7
unknown <sudisimbadav@gmail.com>
2021-03-17 13:57:47 +0200

    added mongoose and its database configurations

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import products from './data/products.js'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -23,5 +27,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server started in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
